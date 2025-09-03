1394983ecac21bfff6d3dcdca0c823e5742a02b3
inonewater <dlsrk5713@naver.com>
2025-09-03 20:09:53 +0900

    Update tx_code.sh

--- SW/user/user_raspi/Telecom/usr_comm/tx_code.sh
+++ SW/user/user_raspi/Telecom/usr_comm/tx_code.sh
@@ -1,6 +1,6 @@
 #Note: This code is intended for review purposes only and is not meant to be executed.
 #video tx udp port: 5602, video stream_id: 0
-#audio tx udp port: 7003, audio stream_id: 49_downlink
+#audio tx udp port: 7003, audio stream_id: 49_uplink
 
 #video tx code
 sudo wfb_tx -f data -p 0 -u 5602 -K /etc/usr.key -B 20 -G long -S 1 -L 1 -M 1 -k 3 -n 6 -i 7669206 wlan1
